0c5a6fe61ebf22379c9129b4e7684dd9f99cfaa9
Julio Carrió <julio.carrio@gmail.com>
2023-07-17 12:59:51 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="card">
-        <h1>Título do item</h1>
-        <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png" />
+      <div className="container">
+        <div className="card">
+          <h1>Rick</h1>
+          <img src="https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png" />
+        </div>
+        <div className="card">
+          <h1>Morty</h1>
+          <img src="https://static.tvtropes.org/pmwiki/pub/images/morty_smith_2.png" />
+        </div>
+        <div className="card">
+          <h1>Jerry</h1>
+          <img src="https://static.tvtropes.org/pmwiki/pub/images/jerry_smith.png" />
+        </div>
+        <div className="card">
+          <h1>Beth</h1>
+          <img src="https://static.tvtropes.org/pmwiki/pub/images/beth_smile.png" />
+        </div>
       </div>
     </>
   )
